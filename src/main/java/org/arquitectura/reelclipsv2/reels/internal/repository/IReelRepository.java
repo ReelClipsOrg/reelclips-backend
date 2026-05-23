6d7c7d6c742f1b3ce12cc2c4a49676370b0aea26
Eduard Meza <eduardmesa@unisabana.edu.co>
2026-05-23 13:36:09 -0500

    Corrección del endpoint api/feed

--- src/main/java/org/arquitectura/reelclipsv2/reels/internal/repository/IReelRepository.java
+++ src/main/java/org/arquitectura/reelclipsv2/reels/internal/repository/IReelRepository.java
@@ -1,12 +1,14 @@
 package org.arquitectura.reelclipsv2.reels.internal.repository;
 
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.arquitectura.reelclipsv2.reels.internal.model.Reel;
 import org.arquitectura.reelclipsv2.shared.enums.EstadoReel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface IReelRepository extends JpaRepository<Reel, Long> {
+    @EntityGraph(attributePaths = {"categorias", "canal", "canal.usuario"})
     List<Reel> findByEstado(EstadoReel estado);
     List<Reel> findByCanalId(Long canalId);
     List<Reel> findByCanalIdAndEstado(Long canalId, EstadoReel estado);
