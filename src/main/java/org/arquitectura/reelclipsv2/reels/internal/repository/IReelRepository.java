9e97a9a4dd58e0863a41ddf75572b9af43305eff
Eduard Meza <eduardmesa@unisabana.edu.co>
2026-05-26 08:42:10 -0500

    Correción de error lazy en endpoint get reels id

--- src/main/java/org/arquitectura/reelclipsv2/reels/internal/repository/IReelRepository.java
+++ src/main/java/org/arquitectura/reelclipsv2/reels/internal/repository/IReelRepository.java
@@ -6,11 +6,21 @@ import org.arquitectura.reelclipsv2.reels.internal.model.Reel;
 import org.arquitectura.reelclipsv2.shared.enums.EstadoReel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface IReelRepository extends JpaRepository<Reel, Long> {
     @EntityGraph(attributePaths = {"categorias", "canal", "canal.usuario"})
+    Optional<Reel> findById(Long id);
+
+    @EntityGraph(attributePaths = {"categorias", "canal", "canal.usuario"})
     List<Reel> findByEstado(EstadoReel estado);
+
+    @EntityGraph(attributePaths = {"categorias", "canal", "canal.usuario"})
     List<Reel> findByCanalId(Long canalId);
+
+    @EntityGraph(attributePaths = {"categorias", "canal", "canal.usuario"})
     List<Reel> findByCanalIdAndEstado(Long canalId, EstadoReel estado);
+
+    @EntityGraph(attributePaths = {"categorias", "canal", "canal.usuario"})
     List<Reel> findByCanalUsuarioIdNot(Long usuarioId);
 }
