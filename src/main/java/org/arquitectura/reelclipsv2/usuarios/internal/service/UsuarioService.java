0edb6d703b46ce50cea2b88538323281df0194a8
Eduard Meza <eduardmesa@unisabana.edu.co>
2026-05-26 10:07:41 -0500

    El registro de usuario ahora es transaccional

--- src/main/java/org/arquitectura/reelclipsv2/usuarios/internal/service/UsuarioService.java
+++ src/main/java/org/arquitectura/reelclipsv2/usuarios/internal/service/UsuarioService.java
@@ -14,6 +14,7 @@ import org.arquitectura.reelclipsv2.usuarios.internal.repository.ICanalRepositor
 import org.arquitectura.reelclipsv2.usuarios.internal.repository.IUsuarioRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
@@ -29,6 +30,7 @@ public class UsuarioService {
     private final SupabaseStorageService storageService;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public UsuarioInfo registrar(String username, String email, String password) {
         if (usuarioRepo.existsByEmail(email)) {
             throw new ReglaNegocioException("El email ya esta registrado: " + email);
